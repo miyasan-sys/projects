ae81aeee1cf3cd107967cf98080fd0ba2a179429
miyasan-sys <miya.miyaw.69@gmail.com>
2026-02-19 20:44:22 +0800

    eto try again

--- system/trashbin.py
+++ system/trashbin.py
@@ -1,24 +1,29 @@
 import datetime
 import pathlib
 
-
 def duration(source, limit): # the duration
     folder = pathlib.Path(source)
+    
+    if not folder.exists():
+        print("Folder not found!")
+        return 0
+
     ngayon = datetime.datetime.now()
     timer = ngayon - datetime.timedelta(minutes=limit)
     count = 0
 
-    for screenshots in folder.iterdir(): #check if the file is expired or not
+    for screenshots in folder.iterdir(): # check if the file is expired or not
         if screenshots.is_file():
             ftime = datetime.datetime.fromtimestamp(screenshots.stat().st_mtime)
+            
             if ftime < timer:
-                print("rm " + screenshots.name)
+                screenshots.unlink
+                print("Deleted: " + screenshots.name)
                 count += 1
         else:
-            print("no directories")
+            print("Skipped directory: " + screenshots.name)
+            
     return count
 
 duration("/home/miyasan/Pictures/Screenshots", 10)
-
-
         
